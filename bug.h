0ac0b1e935bf9057b04f9633f1a3bf540b2ba819
rkh4 <rkh4@aber.ac.uk>
2015-03-16 22:58:21 +0000

    added bug.h

---
+++ bug.h
@@ -0,0 +1,41 @@
+using namespace std;
+#include <utility>
+
+//Base class
+class bug {
+public:
+	bug();
+	int pos[2];
+	int x_pos = pos[0];
+	int y_pos = pos[1];
+	void setX(int x_pos);
+	void setY(int y_pos);
+	int getX();
+	int getY();
+	pair<int, int> getPos();
+	void update();
+	~bug();
+};
+
+
+
+class aphid : public bug {
+public:
+	aphid();
+	int moveDirection();
+	void update();
+	~aphid();
+};
+
+class ladybug : public bug {
+private:
+	int pos[2];
+	int initialDirection;
+public:
+	ladybug();
+	int initialDirection();
+	moveDirection();
+	int moveDirection;
+	void update();	
+	~ladybug();
+};
